a2fabbd6a710ecf4f7553f593350c1436710917e
smsdm4 <m_mirzadeh4@icloud.com>
2023-12-06 16:12:48 +0330

    Project added

---
+++ EnhancedRest/EnhancedRest/EnhancedRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedRestApp.swift
+//  EnhancedRest
+//
+//  Created by Mojtaba Mirzadeh on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
